9e89f40760b90235554fae98b393340995b9679f
harrison <harrison.vanderbyl@gmail.com>
2026-09-06 14:59:42 +1000

    move godot thing to own repo

--- godot_ext_particles/src/register_types.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <godot_cpp/core/class_db.hpp>
-
-void initialize_fluid_particles_module(godot::ModuleInitializationLevel p_level);
-void uninitialize_fluid_particles_module(godot::ModuleInitializationLevel p_level);
